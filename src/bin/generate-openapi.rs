2714c874a76e8813c1a272e867cb914667a20ec9
JerryImMouse <toperkas@gmail.com>
2026-08-29 20:59:25 +0500

    fix: project build without openapi

--- src/bin/generate-openapi.rs
+++ src/bin/generate-openapi.rs
@@ -1,26 +1,39 @@
-extern crate mantle;
-use mantle::web::{
-    account::openapi as account, auth::openapi as auth, discord::openapi as discord,
-    health::openapi as health, metadata::openapi as metadata,
-};
-use utoipa::{OpenApi, openapi::OpenApi as OpenApiImpl};
-
+#[cfg(not(feature = "openapi"))]
 fn main() {
-    let doc = collect_docs();
-
-    std::fs::write(
-        "openapi.json",
-        serde_json::to_string_pretty(&doc).expect("failed to serialize doc"),
-    )
-    .expect("failed to write openapi.json");
+    println!("openapi feature is required");
 }
 
-fn collect_docs() -> OpenApiImpl {
-    let mut doc = mantle::web::openapi::ApiDoc::openapi();
-    doc.merge(health::ApiDoc::openapi());
-    doc.merge(auth::ApiDoc::openapi());
-    doc.merge(discord::ApiDoc::openapi());
-    doc.merge(metadata::ApiDoc::openapi());
-    doc.merge(account::ApiDoc::openapi());
-    doc
+#[cfg(feature = "openapi")]
+fn main() {
+    internal::generate();
+}
+
+#[cfg(feature = "openapi")]
+mod internal {
+    extern crate mantle;
+    use mantle::web::{
+        account::openapi as account, auth::openapi as auth, discord::openapi as discord,
+        health::openapi as health, metadata::openapi as metadata,
+    };
+    use utoipa::{OpenApi, openapi::OpenApi as OpenApiImpl};
+
+    pub fn generate() {
+        let doc = collect_docs();
+
+        std::fs::write(
+            "openapi.json",
+            serde_json::to_string_pretty(&doc).expect("failed to serialize doc"),
+        )
+        .expect("failed to write openapi.json");
+    }
+
+    fn collect_docs() -> OpenApiImpl {
+        let mut doc = mantle::web::openapi::ApiDoc::openapi();
+        doc.merge(health::ApiDoc::openapi());
+        doc.merge(auth::ApiDoc::openapi());
+        doc.merge(discord::ApiDoc::openapi());
+        doc.merge(metadata::ApiDoc::openapi());
+        doc.merge(account::ApiDoc::openapi());
+        doc
+    }
 }
